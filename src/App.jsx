eb0f8a9ed44c16c0470b1a7b54c74a60a68a5949
MirsalehSalayev <saalayevsaleh@gmail.com>
2023-11-25 01:38:56 +0400

    structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './Sections/Navbar';
+import Footer from './Sections/Footer'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.scss"
+
+const Card = () => {
+    return (
+        <div className='Card'>
+            <p className='CardText'>Card</p>
+        </div>
+    )
+}
+
+export default Card
